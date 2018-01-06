083514771a128043e9d00f0f932aff0b6efcd0ac
wuqiwei <1058633117@qq.com>
2018-01-06 15:08:39 +0800

    shell

---
+++ linux-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#杀死tomcat
+tomcat_home=/usr/local/tomcat
+kill -9 $(echo $(netstat -anp|grep 8080) | cut -d " " -f 7|cut -d / -f 1)
+sleep 3
+git pull
+mvn clean compile war:war
+mv target/*.war target/ROOT.war
+rm -rf $tomcat_home/webapps/ROOT*
+mv target/ROOT.war $tomcat_home/webapps/
+$tomcat_home/bin/startup.sh
+tail -n 200 -f $tomcat_home/logs/catalina.out
---
+++ mac-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+tomcat_home=/usr/local/tomcat
+kill -9 $(lsof -i tcp:8080 | sed -n '2p' | awk '{print $2}')
+sleep 3
+git pull
+mvn clean compile war:war
+mv target/*.war target/ROOT.war
+rm -rf $tomcat_home/webapps/ROOT*
+mv target/ROOT.war $tomcat_home/webapps/
+$tomcat_home/bin/startup.sh
+tail -n 200 -f $tomcat_home/logs/catalina.out
